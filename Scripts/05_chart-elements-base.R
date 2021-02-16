037f53738d7780cf564c122989264095771f7e43
Aaron Chafetz <achafetz@usaid.gov>
2021-02-16 13:33:59 -0500

    add in chart elements plot from glitr for svg version

---
+++ Scripts/05_chart-elements-base.R
@@ -0,0 +1,33 @@
+## PROJECT: pretty_in_gray40k
+## AUTHOR:  A.Chafetz & T.Essam | USAID
+## PURPOSE: makeover plots for style guide
+## DATE:    2021-02-16
+## UPDATED: 
+## NOTES:   adapted from glitr
+
+# DEPENDENCIES ------------------------------------------------------------
+
+library(tidyverse)
+library(scales)
+library(glitr)
+library(extrafont)
+library(svglite)
+
+
+# CHART ELEMENTS ----------------------------------------------------------
+
+
+tibble(x = c("FY50Q1", "FY50Q2", "FY50Q3", "FY50Q4"),
+       y = seq(0, 1200, length.out = 4)) %>%
+  ggplot(aes(x, y)) +
+  scale_y_continuous(label = comma) +
+  labs(x = NULL, y = NULL,
+       title = "TITLE",
+       subtitle = "caption/description",
+       caption = "data source") +
+  si_style()
+
+si_save("chart_elements.svg",
+       path = "Graphics",
+       height = 3.7633,
+       width = 4.9306)
