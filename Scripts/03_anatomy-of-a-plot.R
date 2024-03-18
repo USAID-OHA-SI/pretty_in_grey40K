d12cf6e8e3b48fd4e05e5ded7315184e9c7edeb8
Aaron Chafetz <achafetz@usaid.gov>
2024-03-18 11:12:18 -0400

    fix: add ref id, replace colors, update graphic titles

--- Scripts/03_anatomy-of-a-plot.R
+++ Scripts/03_anatomy-of-a-plot.R
@@ -1,21 +1,26 @@
 ## PROJECT: pretty_in_gray40k
 ## AUTHOR:  A.Chafetz & T.Essam | USAID
 ## PURPOSE: anatomy of a plot
+## REF ID:   8b8b72ce 
 ## DATE:    2020-11-03
-## UPDATED: 
+## UPDATED: 2024-03-18
 
 
 # DEPENDENCIES ------------------------------------------------------------
 
-library(fs)
 library(tidyverse)
 library(scales)
-library(glitr)
-library(extrafont)
+library(gagglr)
+library(systemfonts)
 library(glue)
 library(ggtext)
-library(svglite)
 
+# GLOBAL VARIABLES --------------------------------------------------------
+
+  ref_id <- "8b8b72ce"  #a reference to be places in viz captions 
+ 
+  main_fill <- hw_electric_indigo
+  decline_fill <- hw_orchid_bloom
 
 # MUNGE DATA --------------------------------------------------------------
 
@@ -24,7 +29,7 @@ df_index <- hts %>%
          modality %in% c("Index", "IndexMod"),
          period_type == "results") %>% 
   count(operatingunit, indicator, period, wt = value, name = "results") %>% 
-  mutate(fill_col = ifelse(period == "FY50Q1", "#e07653", "#1e87a5"))
+  mutate(fill_col = ifelse(period == "FY50Q1", decline_fill, main_fill))
   
 # PLOT --------------------------------------------------------------------
 
@@ -37,11 +42,12 @@ df_index %>%
                      breaks = seq(0, 8000, by = 1000)) +
   scale_fill_identity() +
   labs(x = NULL, y = NULL,
-       caption = "Source: FY50Q1 MSD",
-       title = "DESPITE <span style = 'color:#1e87a5'>GAINS IN FY49</span>, THE NUMBER OF POSITIVE PATIENTS <br>IDENTIFIED THROUGH INDEX TESTING <span style = 'color:#e07653'>FELL IN FY50Q1</span> "
+       caption = "Source: FY61Q1i Faux Training MSD | 2024-03-18 | Ref id: 8b8b72ce",
+       title = "Index testing falls in FY50Q1" %>% toupper,
+       subtitle = glue("Despite gains <span style = 'color:{main_fill}'> in FY49</span>, the number of positive patients <br> identified through index testing fell <span style = 'color:{decline_fill}'> in FY50Q1 </span>")
        ) +
   si_style_ygrid() +
-  theme(plot.title = element_markdown())
+  theme(plot.subtitle = element_markdown())
 
 
 # EXPORT ------------------------------------------------------------------
@@ -54,7 +60,5 @@ df_index %>%
   p_h <- p_w * (3.6/5.45)
 
 #save
-  # ggsave("Images/anatomy_plot.png", dpi = 300, height = p_h, width = p_w)
-  ggsave("Images/anatomy_plot.svg", dpi = 300, height = p_h, width = p_w)
+  si_save("Graphics/anatomy_plot.svg", height = p_h, width = p_w)
 
-si_save()
