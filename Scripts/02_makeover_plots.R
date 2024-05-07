318f3ba6b2ad6996b9c8dd591cb936e366a324f5
Aaron Chafetz <achafetz@usaid.gov>
2024-05-07 11:34:09 -0400

    update partners and table

--- Scripts/02_makeover_plots.R
+++ Scripts/02_makeover_plots.R
@@ -2,7 +2,8 @@
 ## AUTHOR:  A.Chafetz & T.Essam | USAID
 ## PURPOSE: makeover plots for style guide
 ## DATE:    2020-10-25
-## UPDATED: 2020-02-14
+## UPDATED: 2024-05-06
+# REF ID:   1251f63e 
 
 
 # DEPENDENCIES ------------------------------------------------------------
@@ -10,6 +11,7 @@
 library(tidyverse)
 library(scales)
 library(glitr)
+library(themask)
 library(ggrepel)
 library(systemfonts)
 library(fs)
@@ -22,8 +24,14 @@ library(patchwork)
 # GLOBAL VARIABLES --------------------------------------------------------
 
   #create outputfolder if not created already
-    dir_create("Images")
+    dir_create("Graphics")
 
+  #reference id
+  ref_id <- "1251f63e"
+  
+  #caption
+  source <- glue("Source: FY50Q1c MSD | Ref id: {ref_id}")
+  
   #default Excel color palette
     pal_excel <- c("#4E81BE", "#C1514E", "#9CBB59", "#8165A3", "#4AABC5", "#F79647")
 
@@ -33,8 +41,9 @@ library(patchwork)
   #Excel style
     excel_style <- function(){
       theme_minimal() +
-      theme(text = element_text(family = "Calibri", size = 9, color = "#595959"),
-            plot.title = element_text(family = "Calibri", size = 14, hjust = .5),
+      theme(plot.title = element_text(family = "Calibri", size = 14, hjust = .5),
+            text = element_text(#family = "Calibri", 
+              size = 9, color = "#595959"),
             panel.background = element_blank(),
             panel.grid.major.y = element_line(color = "#D9D9D9"),
             panel.grid.minor = element_blank(),
@@ -43,100 +52,104 @@ library(patchwork)
             axis.line = element_line(color = "#D9D9D9"),
             legend.position = "bottom")
     }
+    
+    
+# PARTNER MAPPING ---------------------------------------------------------
+
+    ptnrs <- unique(cascade$prime_partner_name)
+    
+    set.seed(92)
+    ptnrs_new <- minoria_mechs %>% 
+      slice_sample(n = length(ptnrs)) %>% 
+      pull()
+    
+    ptnrs_map <- tibble(prime_partner_name = ptnrs,
+                        prime_partner = ptnrs_new)
+    
+
+# LOAD DATA ---------------------------------------------------------------
+
+  cascade_alt <- cascade
+    
+  cascade_alt <- cascade_alt %>% 
+    left_join(ptnrs_map, by = join_by(prime_partner_name)) %>% 
+    mutate(prime_partner_name = prime_partner) %>% 
+    select(-prime_partner)
+    
 # LINE --------------------------------------------------------------------
 
-#
-#   glimpse(cascade)
-#
-#   cascade %>%
-#     filter(indicator == "TX_NEW",
-#            period_type == "results",
-#     ) %>%
-#     mutate(label = case_when(period == max(period) ~ primepartner)) %>%
-#     ggplot(aes(period, value, group = primepartner, color = primepartner)) +
-#     geom_line() +
-#     geom_label_repel(aes(label = label))
-
-  sel_ptnrs <- cascade %>%
+  sel_ptnrs <- cascade_alt %>%
     filter(indicator == "TX_NEW",
            period == "FY50Q1") %>%
     arrange(desc(value)) %>%
-    slice(n = 3:7) %>%
-    pull(primepartner)
+    slice(3:7) %>%
+    pull(prime_partner_name)
 
-  df_line <- cascade %>%
+  df_line <- cascade_alt %>%
     filter(indicator == "TX_NEW",
            period_type == "results",
-           primepartner %in% sel_ptnrs) %>%
-    mutate(focus = primepartner == "Ursa Major",
+           prime_partner_name %in% sel_ptnrs) %>%
+    mutate(focus = prime_partner_name == "Jumbo Shrimp",
            label = case_when(period %in% c("FY49Q1", "FY49Q4") & focus == TRUE ~ value))
+   
 
-  write_csv(df_line, "../../../Downloads/default_line.csv", na = "")
+  write_csv(df_line, "Data_public/line.csv", na = "")
 
   # df_line %>%
-  #   ggplot(aes(period, value, group = primepartner, color = primepartner)) +
-  #   geom_line(size = 1.2) +
+  #   ggplot(aes(period, value, group = prime_partner_name, color = prime_partner_name)) +
+  #   geom_line(linewidth = 1.2) +
   #   expand_limits(y = c(0, 1400)) +
-  #   scale_color_manual(values = pal_excel) +
+  #   scale_color_manual(values = pal_excel, guide = "legend") +
   #   scale_y_continuous(expand = c(0, 0), breaks = seq(from = 0, to = 1400, by = 200)) +
   #   scale_x_discrete(expand = c(0, .5)) +
-  #   labs(x = NULL, y = NULL, title = "Saturn Parnter Trends in TX_NEW",
+  #   labs(x = NULL, y = NULL, title = "Saturn Partner Trends in TX_NEW",
   #        color = NULL
   #        ) +
   #   excel_style()
-  #
-  #
-  # # ggsave("Images/line_default.png", dpi = 300, height = 4.71, width = 7.31)
-  # ggsave("Images/line_default.svg", dpi = 300, height = 4.71, width = 7.31)
+
+  # si_save("Graphics/line_default.svg", dpi = 300, height = 4.71, width = 7.31)
 
 
   df_line %>%
-    ggplot(aes(period, value, group = primepartner, color = focus)) +
-    geom_line(size = 1) +
-    geom_line(data = filter(df_line, focus == TRUE), size = 1.2) +
+    ggplot(aes(period, value, group = prime_partner_name, color = focus)) +
+    geom_line(linewidth = 1) +
+    geom_line(data = filter(df_line, focus == TRUE), linewidth = 1.2) +
     geom_label(aes(label = comma(label),
                    vjust = ifelse(period == "FY49Q1", 1.2, -.2)),
                label.size = 0, family = "Source Sans Pro", alpha = .6,
                na.rm = TRUE) +
     expand_limits(y = c(0, 1400)) +
-    scale_color_manual(values = c("gray80", usaid_medblue)) +
+    scale_color_manual(values = c("gray80", hw_electric_indigo)) +
     scale_y_continuous(expand = c(0, 0), labels = comma,
                        breaks = seq(from = 0, to = 1400, by = 200)) +
-    scale_x_discrete(expand = c(0, .25)) +
+    scale_x_discrete(breaks = unique(df_line$period)[grep("Q(1|3)", unique(df_line$period))],
+                     expand = c(0, .25)) +
     labs(x = NULL, y = NULL,
-         title = "LARGE TREATMENT GAINS MADE BY<br><span style = 'color:#0067B9;'>URSA MAJOR</span> IN FY49",
+         title = glue("LARGE TREATMENT GAINS MADE BY<br><span style = 'color:{hw_electric_indigo};'>JUMBO SHRIMP</span> IN FY49"),
          color = NULL,
-         caption = glue("Source: {max(cascade$period)} MSD")
+         caption = source
     ) +
     si_style_ygrid() +
     theme(plot.title = element_markdown(),
           legend.position = "none")
 
-  # ggsave("Images/line_remake.png", dpi = 300, height = 4.71, width = 7.31)
-  ggsave("Images/line_remake.svg", dpi = 300, height = 4.82, width = 4.9306)
+  si_save("Graphics/line_remake.svg", height = 4.75, width = 4.75)
 
 
 
 # BAR ---------------------------------------------------------------------
 
-
-  glimpse(hts)
-
-  hts %>%
-    filter(indicator == "HTS_TST",
-           period_type == "targets") %>%
-    count(primepartner, wt = value, sort = TRUE)
-
-  df_dual <- cascade %>% 
+  df_dual <- cascade_alt %>% 
     filter(indicator %in% c("HTS_TST", "HTS_TST_POS"),
-           primepartner == "Auriga",
+           prime_partner_name == "Bulls",
            period_type == "results") %>% 
     pivot_wider(names_from = indicator) %>% 
     mutate(positivity = HTS_TST_POS / HTS_TST,
            focus = period %in% c("FY49Q2", "FY49Q3", "FY49Q4", "FY50Q1"),
            focus_pos = case_when(focus == TRUE ~ positivity)) %>% 
     arrange(period)
-  
+
+  write_csv(df_dual, "Data_public/dual-bar.csv", na = "")
   
   df_dual %>%
     ggplot(aes(period, HTS_TST)) +
@@ -147,31 +160,32 @@ library(patchwork)
     scale_y_continuous(expand = c(.005, .005), name = "Quarterly Results",
                        sec.axis = sec_axis(~./800000, name = "Positivity", labels = percent)) +
     expand_limits(y = 1000) +
-    labs(x = NULL, title = "Auriga HTS_TST and Positivity") +
+    labs(x = NULL, title = "Bulls HTS_TST and Positivity") +
     excel_style() +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
   
-  ggsave("Images/bar_default.svg", dpi = 300, height = 4.82, width = 4.9306)
+  si_save("Graphics/bar_default.svg", height = 4.75, width = 4.75)
   
   (v_bar <- df_dual %>% 
       ggplot(aes(period, HTS_TST, fill = focus)) +
       geom_col() +
-      scale_y_continuous(expand = c(.005, .005), labels = comma) +
-      scale_x_discrete(breaks = c("FY48Q1", "FY48Q3", "FY49Q1", "FY49Q3", "FY51Q1"))+
-      scale_fill_manual(values = c(moody_blue_light, moody_blue)) +
+      scale_y_continuous(expand = c(.005, .005), labels = label_number(scale_cut = cut_si(''))) +
+      scale_x_discrete(breaks = unique(df_line$period)[grep("Q(1|3)", unique(df_line$period))],
+                       expand = c(.005, .005))+
+      scale_fill_manual(values = c("#8dbda2", hw_hunter)) + #60% tint
       labs(x = NULL, y = NULL) +
       si_style_ygrid() +
       theme(legend.position = "none")
   )
   
   (v_line <- df_dual %>% 
-      ggplot(aes(period, positivity, group = primepartner)) +
+      ggplot(aes(period, positivity, group = prime_partner_name)) +
       geom_point(aes(color = focus)) +
       geom_path(aes(color = focus)) +
-      scale_color_manual(values = c(trolley_grey, moody_blue)) +
+      scale_color_manual(values = c("#8dbda2", hw_hunter)) +
       scale_y_continuous(expand = c(.005, .005), label = percent_format(1)) +
       labs(x = NULL, y = NULL) +
-      si_style_xline() +
+      si_style_nolines() +
       theme(axis.text.x = element_blank(),
             axis.text.y = element_blank())
   )
@@ -179,77 +193,75 @@ library(patchwork)
   v_line / v_bar +
     plot_layout(heights = c(1, 3)) +
     plot_annotation(
-      title = "AURIGA OPTIMIZING TESTING TO DECREASE TOTAL TESTS<br>WHILE INCREASING POSITIVITY IN <span style = 'color:#8980cb;'>LAST 4 QUARTERS</span>",
-      subtitle = 'Positivity and Total Tests in Saturn',
-      caption = 'Source: FY50Q1 MSD'
+      title = glue("BULLS OPTIMIZED TESTING, DECREASING TESTS<br>WHILE INCREASING POSITIVITY IN <span style = 'color:{hw_hunter};'>LAST 4 QS</span>"),
+      subtitle = 'Positivity and Total Tests in Minoria',
+      caption = source,
+      theme = si_style()
     ) & 
-    si_style_ygrid() & 
     theme(plot.title = element_markdown(),
           legend.position = "none")
   
-  ggsave("Images/bar_makeover.svg", dpi = 300, height = 4.82, width = 4.9306)
-  
-  
+  si_save("Graphics/bar_makeover.svg",height = 4.75, width = 4.75)
+
 
 # STACKED BAR -------------------------------------------------------------
 
 
-    glimpse(cascade)
-
-
-    df_stack <- cascade %>%
+    df_stack <- cascade_alt %>%
       filter(indicator == "TX_NEW",
              period_type == "results",
-             period != "FY50Q1")
+             # period != "FY50Q1"
+             )
 
 
     ptnr_sel <- df_stack %>%
       filter(period == max(period)) %>%
       slice_max(n = 9, order_by = value) %>%
-      pull(primepartner)
+      pull(prime_partner_name)
 
     df_stack <- df_stack %>%
-      filter(primepartner %in% ptnr_sel) %>%
-      mutate(partner_fill = primepartner == "Libra")
+      filter(prime_partner_name %in% ptnr_sel) %>%
+      mutate(partner_fill = prime_partner_name == "IronPigs")
 
 
-    orig_stack <- df_stack %>%
-      ggplot(aes(period, value, fill = primepartner)) +
+    write_csv(df_stack, "Data_public/stacked-bar.csv", na = "")
+    
+    
+    # orig_stack <- 
+      df_stack %>%
+      ggplot(aes(period, value, fill = prime_partner_name)) +
       geom_col() +
       # geom_text(aes(label = value)) +
-      # scale_fill_manual(values = rep(pal_excel, 2), name = NULL) +
-      # excel_style() +
-      labs(title = "TX_NEW in Saturn Partners")
+      labs(title = "TX_NEW for Partners in Minoria")
 
-    # ggsave("Images/stacked_default.png", dpi = 300, height = 4.71, width = 7.31)
-    ggsave("Images/stacked_default.svg", dpi = 300, height = 4.82, width = 4.9306)
+    si_save("Graphics/stacked_default.svg", height = 4.75, width = 4.75)
 
 
-    rmk_stack <- df_stack %>%
+    # rmk_stack <- 
+      df_stack %>%
       ggplot(aes(period, value, fill = partner_fill)) +
       geom_col(width = .8) +
-      geom_hline(yintercept = 0) +
-      facet_wrap(~factor(primepartner, ptnr_sel)) +
+      geom_hline(yintercept = 0, color = light_grey) +
+      facet_wrap(~factor(prime_partner_name, ptnr_sel)) +
       scale_y_continuous(label = comma) +
       scale_x_discrete(breaks = c("FY48Q1", "FY49Q1", "FY50Q1")) +
-      scale_fill_manual(values = c("gray70", scooter)) +
+      scale_fill_manual(values = c("gray70", hw_viking)) +
       labs(x = NULL, y = NULL,
-           title = "<span style = 'color:#1e87a5;'>LIBRA </span> SAW POSITIVE TX_NEW GROWTH IN Q1<br> WHILE OTHERS FALTERED",
-           caption =  glue("Source: {max(hts$period)} MSD")
+           title = glue("<span style = 'color:{hw_viking};'>IRONPIGS</span> SAW POSITIVE NEW ON TREATMENT<br>GROWTH IN Q1 WHILE OTHERS SLOWED"),
+           caption =  source
              ) +
       si_style_nolines() +
       theme(legend.position = "none",
             panel.spacing = unit(1, "lines"),
             plot.title = element_markdown())
 
-    # ggsave("Images/stacked_makeover.png", dpi = 300, height = 4.71, width = 7.31)
-    ggsave("Images/stacked_makeover.svg", dpi = 300,  height = 4.82, width = 4.9306)
+    si_save("Graphics/stacked_makeover.svg", height = 4.75, width = 4.75)
     
-    h = 4.82
-    
-    orig_stack + plot_spacer() + rmk_stack +
-      plot_layout(widths = unit(c(4.9306 , .25, 4.9306), 'in'), heights = unit(4.82, 'in'))
-    
-    ggsave("Images/stacked_duo.svg", dpi = 300, width = 10.1112, height = 4.82)
+    # h = 4.82
+    # 
+    # orig_stack + plot_spacer() + rmk_stack +
+    #   plot_layout(widths = unit(c(4.9306 , .25, 4.9306), 'in'), heights = unit(4.82, 'in'))
+    # 
+    # si_save("Graphics/stacked_duo.svg", dpi = 300, width = 10.1112, height = 4.82)
     
     
--- Scripts/06_table_makeover_styleguide.R
+++ Scripts/06_table_makeover_styleguide.R
@@ -1,71 +1,111 @@
+## PROJECT: pretty_in_gray40k
+## AUTHOR:  A.Chafetz & T.Essam | USAID
+## PURPOSE: makeover table for style guide
+## DATE:    2021-02-16
+## UPDATED: 2024-05-07
+## REF ID:  7a608beb 
 
-library(tidyverse)
-library(gt)
-library(glitr)
-library(systemfonts)
-library(systemfontsdb)
+# DEPENDENCIES ------------------------------------------------------------
 
-images <- "Images"
+  library(tidyverse)
+  library(glitr)
+  library(themask)
+  library(systemfonts)
+  library(gt)
+  library(gtExtras)
 
-tbl <- tibble::tribble(
-  ~Agency,      ~Partner,    ~Indicator,       ~Q1,       ~Q2,      ~Q3,      ~Q4,    ~Total,  ~Targets, ~Achievement,
-  "Agency A",      "Boötes",     "HTS_TST",      1500,      2080,     1800,     1050,      6430,     13550,         0.47,
-  "Agency A",      "Boötes", "HTS_TST_POS",       330,       420,      340,      330,      1420,       640,         2.22,
-  "Agency A",      "Boötes",  "Positivity",      0.22,       0.2,     0.19,     0.31,      0.22,      0.05,           NA,
-  "Agency A",      "Auriga",     "HTS_TST",     61470,     57310,    44340,    28720,    191840,    151790,         1.26,
-  "Agency A",      "Auriga", "HTS_TST_POS",      3230,      3270,     3050,     2180,     11730,      5020,         2.34,
-  "Agency A",      "Auriga",  "Positivity",      0.05,      0.06,     0.07,     0.08,      0.06,      0.03,           NA,
-  "Agency B",     "Cepheus",     "HTS_TST",      9870,     10860,    10190,    10560,     41480,     41020,         1.01,
-  "Agency B",     "Cepheus", "HTS_TST_POS",       680,       770,      750,     1180,      3380,      1490,         2.27,
-  "Agency B",     "Cepheus",  "Positivity",      0.07,      0.07,     0.07,     0.11,      0.08,      0.04,           NA,
-  "Agency B", "Capricornus",     "HTS_TST",     31560,     36680,    30850,    29710,    128800,    106000,         1.22,
-  "Agency B", "Capricornus", "HTS_TST_POS",      1970,      2420,     2320,     2700,      9410,      5150,         1.83,
-  "Agency B", "Capricornus",  "Positivity",      0.06,      0.07,     0.08,     0.09,      0.07,      0.05,           NA,
-  "Totals",       "All Partners",     "HTS_TST",    104400,    106930,    87180,    70040,    368550,    312360,         1.18,
-  "Totals",       "All Partners", "HTS_TST_POS",      6210,      6880,     6460,     6390,     25940,     12300,         2.11,
-  "Totals",       "All Partners",  "Positivity",      0.06,      0.06,     0.07,     0.09,      0.07,      0.04,           NA
-)
+# GLOBAL VARIABLES --------------------------------------------------------
 
-
-
-tbl <- tbl %>% 
-  gt(groupname_col = "Agency",
-     rowname_col = "Partner") %>% 
-  fmt_number(rows = Indicator %in% c("HTS_TST", "HTS_TST_POS"),
-             columns = 4:9,
-             decimals = 0) %>% 
-  fmt_percent(rows = Indicator == "Positivity",
-              columns = 4:9,
-              decimals = 0) %>% 
-  fmt_percent(columns = "Achievement",
-              decimals = 0) %>% 
-  tab_style(style = cell_fill(genoa_light, alpha = 0.5),
-            locations = cells_body(
-              columns = c(4:5, 7:8),
-              rows = Indicator == "Positivity" & Partner == "Boötes"
-            )) %>% 
-  tab_style(style = cell_fill(old_rose_light, alpha = 0.5),
-            locations = cells_body(
-              columns = "Achievement",
-              rows = Achievement < 1
-            )) %>% 
-  tab_header("OVERALL TESTING TARGETS SURPASSED IN FY49 ON SATURN") %>% 
-  tab_spanner(
-    label = "Period of Performance",
-    columns = vars(`Q1`, `Q2`, `Q3`, `Q4`)
-  ) %>% 
-  fmt_missing(columns = everything(), missing_text = "-") %>% 
-  tab_options(
-    table.font.size = 10,
-    table.font.names = "SourceSansPro-Regular",
-    footnotes.font.size = 8
-  ) %>% 
-  tab_footnote(
-    footnote = "Source: Saturn MSD downloaded on 2050-01-01",
-    locations = cells_title(groups = "title")
-  ) 
-
-gtsave(tbl, file.path(images,"Style_guide_table_remake.png"))
+  #reference id
+  ref_id <- "7a608beb"
   
-library(janitor)
+  #caption
+  source <- str_glue("Source: FY50Q1c MSD | Ref id: {ref_id}")
 
+# IMPORT ------------------------------------------------------------------
+
+  tbl_raw <- read_csv("Data_public/table.csv")
+
+# PARTNER MAPPING ---------------------------------------------------------
+  
+  ptnrs <- unique(cascade$prime_partner_name)
+  
+  set.seed(92)
+  ptnrs_new <- minoria_mechs %>% 
+    slice_sample(n = length(ptnrs)) %>% 
+    pull()
+  
+  ptnrs_map <- tibble(prime_partner_name = ptnrs,
+                      prime_partner = ptnrs_new) %>% 
+    mutate(prime_partner_name = ifelse(prime_partner_name == "Boötes", "Bootes", prime_partner_name))
+  
+  
+  tbl_raw <- tbl_raw %>% 
+    left_join(ptnrs_map, by = join_by(Partner == prime_partner_name)) %>% 
+    mutate(Partner = case_when(Partner == "All Partners" ~ Partner,
+                               TRUE ~ prime_partner) )%>% 
+    select(-prime_partner)
+  
+# TABLE FUNCTION ----------------------------------------------------------
+
+  drkn_clmn_hdr <- function(.data){
+    .data %>%
+      tab_style(
+        style = list(
+          cell_text(color = grey90k)
+        ),
+        locations = cells_column_labels(
+        )
+      )
+  }
+# BUILD TABLE -------------------------------------------------------------
+
+  tbl <- tbl_raw %>%
+    gt(groupname_col = "Agency",
+       rowname_col = "Partner") %>% 
+    fmt_number(rows = Indicator %in% c("HTS_TST", "HTS_TST_POS"),
+               columns = 4:9,
+               decimals = 0) %>% 
+    fmt_percent(rows = Indicator == "Positivity",
+                columns = 4:9,
+                decimals = 0) %>% 
+    fmt_percent(columns = "Achievement",
+                decimals = 0) %>% 
+    tab_style(style = cell_fill("#697ebc", alpha = 0.5), #hw_electric_indigo @ 80 
+              locations = cells_body(
+                columns = c(4:5, 7:8),
+                rows = Indicator == "Positivity" & Partner == "Shuckers"
+              )) %>% 
+    tab_style(style = cell_fill("#f8a27e", alpha = 0.5), #hw_tango @ 60%
+              locations = cells_body(
+                columns = "Achievement",
+                rows = Achievement < 1
+              )) %>% 
+    tab_header("OVERALL TESTING TARGETS SURPASSED IN FY49 ON SATURN") %>% 
+    tab_spanner(
+      label = "Period of Performance",
+      columns = c(`Q1`, `Q2`, `Q3`, `Q4`)
+    ) %>% 
+    sub_missing(columns = everything(), missing_text = "-") %>% 
+    tab_options(
+      table.font.size = 10,
+      table.font.names = "Source Sans Pro",
+      source_notes.font.size = 8
+    ) %>%
+    # gt_theme_nytimes() %>%
+    # tab_options(
+    #   table.font.size = px(10),
+    #   table.font.names = "Source Sans Pro",
+    #   row_group.font.weight = "bold",
+    #   data_row.padding = px(0.5),
+    #   column_labels.font.size = px(12),
+    #   row_group.padding = px(1)) %>%
+    # drkn_clmn_hdr() %>% 
+    tab_source_note(source)
+
+  tbl
+  
+# EXPORT ------------------------------------------------------------------
+
+  gtsave(tbl, "Images/Style_guide_table_remake.png")
+  
